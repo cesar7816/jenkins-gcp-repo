2f944feb92ac7b1f91ff8f16822290daac6e83e4
cesar7816 <e-cecortes@gentera.com.mx>
2022-01-28 22:54:57 -0600

    removing backend

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_compute_address" "ip_address" {
   name = var.ip-addres-2
 }
 resource "google_service_account" "service_account" {
-  account_id   = "service-account-id-1"
-  display_name = "Service Account-1"
+  account_id   = "service-account-id"
+  display_name = "Service Account"
 }
